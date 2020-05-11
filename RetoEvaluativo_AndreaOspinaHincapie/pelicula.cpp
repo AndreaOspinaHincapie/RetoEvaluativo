74a38ad3c94df6f18128a7a46879b0d8cd661fd6
AndreaOspinaHincapie <andreospinah3000@gmail.com>
2020-05-10 20:26:46 -0500

    Metodos y atributos de clase pelicula.

--- RetoEvaluativo_AndreaOspinaHincapie/pelicula.cpp
+++ RetoEvaluativo_AndreaOspinaHincapie/pelicula.cpp
@@ -1,5 +1,105 @@
 #include "pelicula.h"
 
+string Pelicula::getID() const
+{
+    return ID;
+}
+
+void Pelicula::setID(const string &value)
+{
+    ID = value;
+}
+
+string Pelicula::getNombre() const
+{
+    return nombre;
+}
+
+void Pelicula::setNombre(const string &value)
+{
+    nombre = value;
+}
+
+string Pelicula::getGenero() const
+{
+    return genero;
+}
+
+void Pelicula::setGenero(const string &value)
+{
+    genero = value;
+}
+
+string Pelicula::getDuracionMin() const
+{
+    return duracionMin;
+}
+
+void Pelicula::setDuracionMin(const string &value)
+{
+    duracionMin = value;
+}
+
+int Pelicula::getSala() const
+{
+    return sala;
+}
+
+void Pelicula::setSala(int value)
+{
+    sala = value;
+}
+
+string Pelicula::getHora() const
+{
+    return Hora;
+}
+
+void Pelicula::setHora(const string &value)
+{
+    Hora = value;
+}
+
+string Pelicula::getClasificacion() const
+{
+    return clasificacion;
+}
+
+void Pelicula::setClasificacion(const string &value)
+{
+    clasificacion = value;
+}
+
+int Pelicula::getCapacidadMax() const
+{
+    return capacidadMax;
+}
+
+void Pelicula::setCapacidadMax(int value)
+{
+    capacidadMax = value;
+}
+
+int Pelicula::getAsientosDisponibles() const
+{
+    return asientosDisponibles;
+}
+
+void Pelicula::setAsientosDisponibles(int value)
+{
+    asientosDisponibles = value;
+}
+
+map<string, vector<int> > Pelicula::getAsientos() const
+{
+    return Asientos;
+}
+
+void Pelicula::setAsientos(const map<string, vector<int> > &value)
+{
+    Asientos = value;
+}
+
 Pelicula::Pelicula()
 {
 
--- RetoEvaluativo_AndreaOspinaHincapie/pelicula.h
+++ RetoEvaluativo_AndreaOspinaHincapie/pelicula.h
@@ -1,11 +1,49 @@
 #ifndef PELICULA_H
 #define PELICULA_H
 
-
+#include<string>
+#include<map>
+#include<vector>
+using namespace std;
 class Pelicula
 {
+private:
+    string ID;
+    string nombre;
+    string genero;
+    string duracionMin;
+    int sala;
+    string Hora;
+    string clasificacion;
+    int capacidadMax;
+    int asientosDisponibles;
+    map<string,vector<int>> Asientos;
+    void setAsientos(const map<string, vector<int> > &value);
+
+
+
+
 public:
     Pelicula();
+    string getID() const;
+    void setID(const string &value);
+    string getNombre() const;
+    void setNombre(const string &value);
+    string getGenero() const;
+    void setGenero(const string &value);
+    string getDuracionMin() const;
+    void setDuracionMin(const string &value);
+    int getSala() const;
+    void setSala(int value);
+    string getHora() const;
+    void setHora(const string &value);
+    string getClasificacion() const;
+    void setClasificacion(const string &value);
+    int getCapacidadMax() const;
+    void setCapacidadMax(int value);
+    int getAsientosDisponibles() const;
+    void setAsientosDisponibles(int value);
+    map<string, vector<int> > getAsientos() const;
 };
 
 #endif // PELICULA_H
